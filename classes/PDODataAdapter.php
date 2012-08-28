f75078a98ab188e008451aba5c0fe314264d070d
martindiphoorn <martindiphoorn@6945d078-fe45-42e2-ba59-270ac4789834>
2012-08-28 15:20:17 +0000

    Issue #8, class holder for the PDO supported dbases

--- classes/PDODataAdapter.php
+++ classes/PDODataAdapter.php
@@ -1,23 +1,5 @@
 <?php
-class PDODataAdapter extends CsvWriter implements DataAdapter {
-
-    public function writeLiveInfo($invtnum, Live $live);
-    public function readLiveInfo($invtnum);
-    public function dropLiveInfo($invtnum);
-
-    public function writeMaxPowerToday($invtnum, MaxPowerToday $mpt);
-    public function readMaxPowerToday($invtnum);
-    public function dropMaxPowerToday($invtnum);
-
-    public function addHistory($invtnum, Live $live, $date);
-    public function readHistory($invtnum, $date);
-    public function getHistoryCount($intnum, $date);
-
-    public function addEnergy($invtnum, MaxPowerToday $energy, $year);
-
-    public function addAlarm($invtnum, Alarm $alarm);
-    public function readAlarm($invtnum);
-
+class PDODataAdapter implements DataAdapter {
 
     /**
      * write the live info to the file
