41f04a81e8147e99c55224537c0cf47aa0889a94
martindiphoorn <martindiphoorn@6945d078-fe45-42e2-ba59-270ac4789834>
2012-09-24 19:54:00 +0000

    Started creation off the PeriodHelper

---
+++ classes/PeriodHelper.php
@@ -0,0 +1,33 @@
+<?php
+class PeriodHelper {
+    /**
+     * Check if the period has been passed
+     * @param string $name
+     * @param int $interval in minutes
+     * @return boolean
+     */
+    public static function isPeriodJob($name, $interval) {
+        $currenttime = time();
+        $jobname = 'job_' . $name;
+        $minute= date("i");
+        if (($minute % $interval == 0)) {
+            // Retrieve record
+            $bean = R::findOne('cronjob', ' name = :name', array(':name'=>$jobname));
+            if (!$bean){
+                $bean = R::dispense('cronjob');
+                $bean->name = $jobname;
+                $bean->lastrun = 0;
+            }
+
+            if (($currenttime - $bean->lastrun) > ($interval * 60)) {
+                // it's time do something
+                $bean->lastrun = $currenttime;
+                R::store($bean);
+                return true;
+            } else {
+                // not yet time
+                return false;
+            }
+        }
+    }
+}
---
+++ test.php
@@ -0,0 +1,22 @@
+<?php
+include "classes/classloader.php";
+
+$config = new Config;
+R::setup('sqlite:'.$config->dbHost );
+R::debug(false);
+
+
+
+// Test the isPeriodJob
+while (true) {
+    if (PeriodHelper::isPeriodJob('1min', 1)) {
+        echo ('1minjob: ' . date("H:i:s", time()) ."\n");
+    }
+    if (PeriodHelper::isPeriodJob('5min', 5)) {
+        echo ('5minjob: ' . date("H:i:s", time()) ."\n");
+    }
+}
+
+
+
+?>
