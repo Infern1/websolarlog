5c1271dab2233b6f9209bab44d6f0f5d0f5827a7
martindiphoorn <martindiphoorn@6945d078-fe45-42e2-ba59-270ac4789834>
2012-10-03 07:42:12 +0000

    added jqPlot bar renderer to the header

--- template/green/header.php
+++ template/green/header.php
@@ -3,17 +3,17 @@
 <head>
 	<title>WebSolarLog</title>
 	<meta http-equiv="Content-type" content="text/html; charset=utf-8" />
-	
+
 	<link rel="stylesheet" href="css/blueprint/screen.css" type="text/css" media="screen, projection">
     <link rel="stylesheet" href="css/blueprint/print.css" type="text/css" media="print">
     <!--[if lt IE 8]><link rel="stylesheet" href="css/blueprint/ie.css" type="text/css" media="screen, projection"><![endif]-->
-    
+
 	<link rel="shortcut icon" href="template/green/css/images/favicon.ico" />
 	<link rel="stylesheet" href="template/green/css/style.css" type="text/css" media="all" />
 	<link rel="stylesheet" href="css/jquery.jqplot.min.css" type="text/css" />
 	<link rel="stylesheet" href="js/jqueryuicss/jquery-ui-1.8.24.custom.css" type="text/css" />
 	<script src="template/green/js/jquery-1.7.min.js" type="text/javascript"></script>
-	
+
 	<script src="template/green/js/jquery.jcarousel.js" type="text/javascript"></script>
 	<script src="template/green/js/DD_belatedPNG-min.js" type="text/javascript"></script>
 	<script src="template/green/js/functions.js" type="text/javascript"></script>
@@ -22,6 +22,7 @@
 	<script type="text/javascript" src="js/jquery-ui-1.8.24.custom.min.js"></script>
 	<script type="text/javascript" src="js/jquery.jqplot.min.js"></script>
     <script type="text/javascript" src="js/jqplot_plugins/jqplot.json2.min.js"></script>
+    <script type="text/javascript" src="js/jqplot_plugins/jqplot.barRenderer.min.js"></script>
     <script type="text/javascript" src="js/jqplot_plugins/jqplot.canvasTextRenderer.min.js"></script>
     <script type="text/javascript" src="js/jqplot_plugins/jqplot.canvasAxisTickRenderer.min.js"></script>
     <script type="text/javascript" src="js/jqplot_plugins/jqplot.canvasAxisLabelRenderer.min.js"></script>
@@ -33,12 +34,12 @@
     <script type="text/javascript" src="js/jqplot_plugins/jqplot.highlighter.min.js"></script>
     <script language="javascript" type="text/javascript" src="js/websolarlog.js"></script>
     			<style>
-			#tabs { height: 200px; } 
-			.tabs-bottom { position: relative; } 
-			.tabs-bottom .ui-tabs-panel { height: 140px; overflow: auto; } 
-			.tabs-bottom .ui-tabs-nav { position: absolute !important; left: 0; bottom: 0; right:0; padding: 0 0.2em 0.2em 0; } 
+			#tabs { height: 200px; }
+			.tabs-bottom { position: relative; }
+			.tabs-bottom .ui-tabs-panel { height: 140px; overflow: auto; }
+			.tabs-bottom .ui-tabs-nav { position: absolute !important; left: 0; bottom: 0; right:0; padding: 0 0.2em 0.2em 0; }
 			.tabs-bottom .ui-tabs-nav li { margin-top: -2px !important; margin-bottom: 1px !important; border-top: none; border-bottom-width: 1px; }
 			.ui-tabs-selected { margin-top: -3px !important; }
 			</style>
-    
+
 </head>
