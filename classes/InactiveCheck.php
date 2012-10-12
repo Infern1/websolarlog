83a2f228058f8e7c57dba18ca2cbe53319859a12
martindiphoorn <martindiphoorn@6945d078-fe45-42e2-ba59-270ac4789834>
2012-10-12 06:51:46 +0000

    Make the check more solid.

--- classes/InactiveCheck.php
+++ classes/InactiveCheck.php
@@ -15,22 +15,26 @@ class InactiveCheck {
 
     public function check() {
         // Only continue when the sun is up!
-        if (Util::isSunDown($this->config,0)) {
+        // -900, means start checking 15 min after the sun is up and before it is down
+        if (Util::isSunDown($this->config,-900)) {
             return true;
         }
 
-        foreach ($this->config->inverters as $inverter) {
-            $live = $this->adapter->readLiveInfo($inverter->id);
-            if ((time() - $live->time > 3600)) {
-                $event = new Event($inverter->id, time(), 'Check', "Live data for inverter '" . $inverter->name . "' not updated for more then one hour.");
-                $subject = "WSL :: Event message";
-                $body = "Hello, \n\n We have detected an problem on your inverter.\n\n";
-                $body .= $event->event . "\n\n";
-                $body .= "Please check if everything is alright.\n\n";
-                $body .= "WebSolarLog";
+        // Only check every 15 minutes
+        if (PeriodHelper::isPeriodJob("InactiveCheckJob", 15)) {
+            foreach ($this->config->inverters as $inverter) {
+                $live = $this->adapter->readLiveInfo($inverter->id);
+                if ((time() - $live->time > 3600)) {
+                    $event = new Event($inverter->id, time(), 'Check', "Live data for inverter '" . $inverter->name . "' not updated for more then one hour.");
+                    $subject = "WSL :: Event message";
+                    $body = "Hello, \n\n We have detected an problem on your inverter.\n\n";
+                    $body .= $event->event . "\n\n";
+                    $body .= "Please check if everything is alright.\n\n";
+                    $body .= "WebSolarLog";
 
-                $event->alarmSend = Common::sendMail($subject, $body, $this->config);
-                $this->adapter->addEvent($inverter->id, $event);
+                    $event->alarmSend = Common::sendMail($subject, $body, $this->config);
+                    $this->adapter->addEvent($inverter->id, $event);
+                }
             }
         }
     }
