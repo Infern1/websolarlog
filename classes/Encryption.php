77852c40fbbaa373fed9c24aa680d4567ebece7e
martindiphoorn <martindiphoorn@6945d078-fe45-42e2-ba59-270ac4789834>
2012-10-23 19:17:32 +0000

    Added encryption class

--- classes/Encryption.php
+++ classes/Encryption.php
@@ -4,34 +4,26 @@ class Encryption {
     const MODE   = MCRYPT_MODE_CBC;
 
     public static function encrypt($plaintext) {
-        //if (function_exists("mcrypt_module_open")) {
-            $td = mcrypt_module_open(self::CYPHER, '', self::MODE, '');
-            $iv = mcrypt_create_iv(mcrypt_enc_get_iv_size($td), MCRYPT_RAND);
-            mcrypt_generic_init($td, self::getKey(), $iv);
-            $crypttext = mcrypt_generic($td, $plaintext);
-            mcrypt_generic_deinit($td);
-            return base64_encode($iv.$crypttext);
-        //} else {
-        //    return $plaintext;
-        //}
+        $td = mcrypt_module_open(self::CYPHER, '', self::MODE, '');
+        $iv = mcrypt_create_iv(mcrypt_enc_get_iv_size($td), MCRYPT_RAND);
+        mcrypt_generic_init($td, self::getKey(), $iv);
+        $crypttext = mcrypt_generic($td, $plaintext);
+        mcrypt_generic_deinit($td);
+        return base64_encode($iv.$crypttext);
     }
 
     public static function decrypt($crypttext) {
-        if (function_exists("mcrypt_module_open")) {
-            $crypttext = base64_decode($crypttext);
-            $plaintext = '';
-            $td        = mcrypt_module_open(self::CYPHER, '', self::MODE, '');
-            $ivsize    = mcrypt_enc_get_iv_size($td);
-            $iv        = substr($crypttext, 0, $ivsize);
-            $crypttext = substr($crypttext, $ivsize);
-            if ($iv) {
-                mcrypt_generic_init($td, self::getKey(), $iv);
-                $plaintext = mdecrypt_generic($td, $crypttext);
-            }
-            return trim($plaintext);
-        } else {
-
+        $crypttext = base64_decode($crypttext);
+        $plaintext = '';
+        $td        = mcrypt_module_open(self::CYPHER, '', self::MODE, '');
+        $ivsize    = mcrypt_enc_get_iv_size($td);
+        $iv        = substr($crypttext, 0, $ivsize);
+        $crypttext = substr($crypttext, $ivsize);
+        if ($iv) {
+            mcrypt_generic_init($td, self::getKey(), $iv);
+            $plaintext = mdecrypt_generic($td, $crypttext);
         }
+        return trim($plaintext);
     }
 
     public static function getKey() {
