cac249b72fb263846d3d2fe2ddc1dc0278efaab6
martindiphoorn <martindiphoorn@6945d078-fe45-42e2-ba59-270ac4789834>
2012-11-10 13:54:12 +0000

    Improve the inactive check

--- classes/InactiveCheck.php
+++ classes/InactiveCheck.php
@@ -15,20 +15,22 @@ class InactiveCheck {
 
     public function check() {
         // Only continue when the sun is up!
-        // -900, means start checking 15 min after the sun is up and before it is down
-        if (Util::isSunDown($this->config,-900)) {
+        // -1800, means start checking 30 min after the sun is up and before it is down
+        if (Util::isSunDown($this->config,-1800)) {
             return true;
         }
 
-        // Only check every 15 minutes
-        if (PeriodHelper::isPeriodJob("InactiveCheckJob", 15)) {
+        // Only check every 30 minutes
+        if (PeriodHelper::isPeriodJob("InactiveCheckJob", 30)) {
             foreach ($this->config->inverters as $inverter) {
                 $live = $this->adapter->readLiveInfo($inverter->id);
-                if ((time() - $live->time > 3600)) {
+                if (!empty($live->time) && (time() - $live->time > 3600)) {
                     $event = new Event($inverter->id, time(), 'Check', "Live data for inverter '" . $inverter->name . "' not updated for more then one hour.");
                     $subject = "WSL :: Event message";
                     $body = "Hello, \n\n We have detected an problem on your inverter.\n\n";
                     $body .= $event->event . "\n\n";
+                    $body .= "Please check if everything is alright.\n\n";
+                    $body .= "Time of live data : " . date("d-m-Y H:i:s", $live->time) . "\n\n";
                     $body .= "Please check if everything is alright.\n\n";
                     $body .= "WebSolarLog";
 
