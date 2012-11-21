af86ca5b6f94b1631182a35f0de41dd1d43434e4
martindiphoorn <martindiphoorn@6945d078-fe45-42e2-ba59-270ac4789834>
2012-11-21 14:22:32 +0000

    Trying to fix ie8 problems

--- template/green/header.php
+++ template/green/header.php
@@ -22,7 +22,7 @@ $config = Session::getConfig();
 	<link rel="stylesheet" href="js/jqueryuicss/jquery-ui-custom.css" type="text/css" />
 	<link rel="stylesheet" href="template/green/css/custom.css" type="text/css" media="all" />
 	
-	<!--[if lt IE 9]><script language="javascript" type="text/javascript" src="js/excanvas.js"></script><![endif]-->
+	<!--[if lt IE 9]><script language="javascript" type="text/javascript" src="js/excanvas.min.js"></script><![endif]-->
 	<script type="text/javascript" src="js/jquery.min.js"></script>
 	<script type="text/javascript" src="js/jquery.pnotify.min.js"></script>
 	<script type="text/javascript" src="js/handlebars.js"></script>
