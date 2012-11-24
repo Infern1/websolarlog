ca8ca0b461e5e99d095f61017860eeff31bec4df
martindiphoorn <martindiphoorn@6945d078-fe45-42e2-ba59-270ac4789834>
2012-11-24 22:11:09 +0000

    PvOutputAddon

--- addon/PvOutputAddon.php
+++ addon/PvOutputAddon.php
@@ -72,7 +72,7 @@ class PvOutputAddon {
 			$result = curl_exec($ch);
 			$httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
 			curl_close($ch);
-			HookHandler::getInstance()->fire("onDebug", "curl code: " .  $httpCode);
+			HookHandler::getInstance()->fire("onDebug", "send to pvoutput: " . print_r($vars, true));
 			HookHandler::getInstance()->fire("onDebug", "curl result: " .  $result);
 			if ($httpCode == "200") {
 				return true;
