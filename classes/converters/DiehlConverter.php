f7950954d9f7f3a05ce58920615c417f86c4be89
freemann01 <freemann01@6945d078-fe45-42e2-ba59-270ac4789834>
2013-02-22 14:14:38 +0000

    Fix on Diehl KWHT in History tabel (value was saved as 733 where it should be 73.3) Add some extra checks to Diehl converter

--- classes/converters/DiehlConverter.php
+++ classes/converters/DiehlConverter.php
@@ -20,14 +20,16 @@ class DiehlConverter
         	if(is_array($value)){
         		foreach ($value as $keys => $values){
 			        $live->time = strtotime(date("d-m-Y H:i:s"));
-			        if($values->path == 'eNEXUS_0066[s:17,t:1]' ){ $live->I1A = ($values->value);}
-			        if($values->path == 'eNEXUS_0055[s:17,t:1]'){ $live->I1V = ($values->value);}
-			        if($values->path == 'eNEXUS_0065[s:17,t:1]'){ $live->I1P = ($values->value);}
-			        if($values->path == 'eNEXUS_0066[s:17,t:1,p:1]'){ $live->GA = ($values->value);}
-			        if($values->path == 'eNEXUS_0064[s:17,t:1,p:1]'){ $live->GP = ($values->value);}
-        			if($values->path == 'eNEXUS_0009[s:17,t:1,p:1]'){ $live->GV =($values->value);}
-        			if($values->path == 'eNEXUS_0043[s:17,t:1]'){ $live->KWHT =($values->value);}
+			        if($values->path == 'eNEXUS_0066[s:17,t:1]' AND $values->value>0){ $live->I1A = ($values->value);}
+			        if($values->path == 'eNEXUS_0055[s:17,t:1]' AND $values->value>0){ $live->I1V = ($values->value);}
+			        if($values->path == 'eNEXUS_0065[s:17,t:1]' AND $values->value>0){ $live->I1P = ($values->value);}
+			        if($values->path == 'eNEXUS_0066[s:17,t:1,p:1]' AND $values->value>0){ $live->GA = ($values->value);}
+			        if($values->path == 'eNEXUS_0064[s:17,t:1,p:1]' AND $values->value>0){ $live->GP = ($values->value);}
+        			if($values->path == 'eNEXUS_0009[s:17,t:1,p:1]' AND $values->value>0){ $live->GV =($values->value);}
+        			if($values->path == 'eNEXUS_0043[s:17,t:1]' AND $values->value>0){ $live->KWHT =($values->value/10);}
+        			// if GP(GridPower) AND I1P (MPP 1 Power) is bigger than 0
         			if($live->GP > 0 AND $live->I1P > 0){
+        				// We can calculate the Efficienty DC>AC in %
 	        			$live->EFF = ($live->GP / $live->I1P)*100;
         			}
         		}
