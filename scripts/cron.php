3e800ff659ffd4956ed1b3d28dcd8dd5819c65b8
martindiphoorn <martindiphoorn@6945d078-fe45-42e2-ba59-270ac4789834>
2013-02-23 18:57:20 +0000

    Introducing a cronjob script

---
+++ scripts/cron.php
@@ -0,0 +1,24 @@
+<?php
+error_reporting(E_ALL);
+
+$startTime = time(); // start time in seconds
+
+$docRoot = dirname(dirname(__FILE__));
+require_once $docRoot . '/admin/classes/classloader.php';
+Session::initialize();
+
+$workHandler = new WorkHandler();
+while ($startTime - time() < 55) {
+	try {
+		$workHandler->start();
+	} catch (Exception $e) {
+		echo ($e->getMessage());
+	}
+	
+	/*
+	 *  As we are running as cron we probably are on an server with shared hosting
+	 *  We don't want to get the cpu to 100% so sit back for 5 seconds
+	 */
+	sleep(5);
+}
+?>
