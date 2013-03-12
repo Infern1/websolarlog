e75a6384f30faf6ea99bb9df6a77e5c18135a4e6
martindiphoorn <martindiphoorn@6945d078-fe45-42e2-ba59-270ac4789834>
2013-03-12 08:25:13 +0000

    Also update the server.php

--- classes/Updater.php
+++ classes/Updater.php
@@ -106,6 +106,7 @@ class Updater {
 
         // Update a few script files
         copy($source . "scripts/worker.php", $target . "scripts/worker.php");
+        copy($source . "scripts/server.php", $target . "scripts/server.php");
         copy($source . "scripts/wsl.sh", $target . "scripts/wsl.sh");
         chmod($target . "scripts/wsl.sh", "750");
     }
