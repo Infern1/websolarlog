93808c09a9c24b6c98b283cc5142fe637074f744
martindiphoorn <martindiphoorn@6945d078-fe45-42e2-ba59-270ac4789834>
2013-03-29 18:36:51 +0000

    Add comments to the device api

--- classes/DeviceApi.php
+++ classes/DeviceApi.php
@@ -1,7 +1,7 @@
 <?php
 interface DeviceApi {
 	/**
-	 * Returns the state off the inverter
+	 * Returns the state off the device
 	 * We currently support the folowing states:
 	 *   -   0 = Try to detect
 	 *   -   1 = Offline
@@ -9,10 +9,29 @@ interface DeviceApi {
 	 */
 	public function getState();
 	
+	/**
+	 * Try to retrieve an alarm from the device
+	 */
     public function getAlarms();
+    
+    /**
+     * Get live data from the device
+     */
     public function getData();
+    
+    /**
+     * Get information of the device like the model, type, serial, etc
+     */
     public function getInfo();
+    
+    /**
+     * Try to synchronize the time
+     */
     public function syncTime();
+    
+    /**
+     * Try to retrieve the history data off the device
+     */
     public function getHistoryData();
 }
 ?>
