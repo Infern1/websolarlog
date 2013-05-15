262537015fca438b3fea133e094ebca857c99f3d
martindiphoorn <martindiphoorn@6945d078-fe45-42e2-ba59-270ac4789834>
2013-05-15 19:33:53 +0000

    WeatherService added functions from PDODataAdapter

--- classes/services/WeatherService.php
+++ classes/services/WeatherService.php
@@ -2,6 +2,11 @@
 class WeatherService {
 	public static $tbl = "weather";
 	
+	/**
+	 * Save the object to the database
+	 * @param Weather $object
+	 * @return Weather
+	 */
 	public function save(Weather $object) {
 		$bObject = ($object->id > 0) ? R::load(self::$tbl, $object->id) : R::dispense(self::$tbl);
 		$bObject = $this->toBean($object, $bObject);
@@ -9,6 +14,11 @@ class WeatherService {
 		return $object;
 	}
 	
+	/**
+	 * Load an object from the database
+	 * @param int $id
+	 * @return Weather
+	 */
 	public function load($id) {
 		$bObject = R::load(self::$tbl, $id);
 		if ($bObject->id > 0) {
@@ -17,12 +27,49 @@ class WeatherService {
 		return isset($object) ? $object : new Weather();
 	}
 	
-	public function getListByDevice(Inverter $device) {
+	/**
+	 * Retrieve all values for an device
+	 * @param Inverter $device
+	 * @return array of Weather
+	 */
+	public function getArrayByDevice(Inverter $device) {
 		$bObjects = R::find( self::$tbl, ' deviceId = :deviceId ORDER BY time ', array("deviceId"=>$device->id));
 		$objects = array();
 		foreach ($bObjects as $bObject) {
 			$objects[] = $this->toObject($bObject);
 		}
+		return $objects;
+	}
+	
+	/**
+	 * Retrieve the last object
+	 * @param Inverter $device
+	 * @return Weather
+	 */
+	public function getLastWeather(Inverter $device) {
+		$bean = R::findOne('weather',' deviceId = :deviceId ORDER BY time DESC LIMIT 1',array(':deviceId'=>$device->id));
+		return $this->toObject($bean);
+	}
+	
+	/**
+	 * Retrieve all objects for the given device and for the given date (default today)
+	 * @param Inverter $device
+	 * @param string $date
+	 * @return array of Weather
+	 */
+	public function getWeatherForDate(Inverter $device, $date=null) {
+		(!$date)? $date = date('d-m-Y') : $date = $date;
+		$beginEndDate = Util::getBeginEndDate('day', 1,$date);
+	
+		$beans =  R::findAll( 'weather', ' where deviceId = :deviceId AND time > :beginDate AND time < :endDate ORDER BY time',
+				array(':deviceId'=>$device->id,':beginDate'=>$beginEndDate['beginDate'],':endDate'=>$beginEndDate['endDate'])
+		);
+	
+		$objects = array();
+		foreach ($beans as $bean) {
+			$objects[] = $this->toObject($bean);
+		}
+	
 		return $objects;
 	}
 	
@@ -53,7 +100,7 @@ class WeatherService {
 		$object->conditionId = $bObject->conditionId;
 		$object->wind_speed = $bObject->wind_speed;
 		$object->wind_direction = $bObject->wind_direction;
-		return $this;
+		return $object;
 	}
 }
 ?>
