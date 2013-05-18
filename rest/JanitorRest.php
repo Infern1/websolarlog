127cb085b36cd0245a72ff1e87e555ef4c096a52
martindiphoorn <martindiphoorn@6945d078-fe45-42e2-ba59-270ac4789834>
2013-05-18 09:42:30 +0000

    Created an Janitor script which can be started by restfull service

---
+++ rest/JanitorRest.php
@@ -0,0 +1,35 @@
+<?php
+// This script will cleanup mess in the database
+
+class JanitorRest {
+	
+	public function GET($request, $options) {
+		$option = "";
+		if (count($options) > 0) {
+			$option = (trim($options[0]) != "") ? strtolower($options[0]) : $option;
+		}
+		
+		if ($option != "clean" && $option != "DbCheck") {
+			exit();
+		}
+		
+		$item = new QueueItem(time(), "JanitorRest." . $option, null,	 false, 0, true);
+		QueueServer::addItemToDatabase($item);
+		
+		return array("status"=>"added to queue");
+	}
+	
+	
+	public function clean() {
+		HookHandler::getInstance()->fire('onInfo', "Janitor starts cleaning.");
+		HookHandler::getInstance()->fire("onJanitorClean");
+	}
+	
+	public function DbCheck() {
+		HookHandler::getInstance()->fire('onInfo', "Janitor checking the datbase.");
+		HookHandler::getInstance()->fire("onJanitorDbCheck", "before");
+		HookHandler::getInstance()->fire("onJanitorDbCheck", "check");
+		HookHandler::getInstance()->fire("onJanitorDbCheck", "after");
+	}
+}
+?>
