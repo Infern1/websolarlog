8fb9f3d1d6aab0dc17584f1073bff13d84217cbc
martindiphoorn <martindiphoorn@6945d078-fe45-42e2-ba59-270ac4789834>
2013-05-22 09:01:38 +0000

    Removed old code

--- utils/cron.php
+++ utils/cron.php
@@ -7,21 +7,6 @@ $docRoot = dirname(dirname(__FILE__));
 require_once $docRoot . '/classes/classloader.php';
 Session::initialize();
 
-$workHandler = new WorkHandler();
-while (time() - $startTime < 55) {
-	try {
-		$workHandler->start();
-	} catch (Exception $e) {
-		echo ($e->getMessage());
-	}
-	
-	/*
-	 *  As we are running as cron we probably are on an server with shared hosting
-	 *  We don't want to get the cpu to 100% so sit back for 5 seconds
-	 */
-	sleep(5);
-}
-
 $historyUpdateRate = 5 * 60; // 5 minute refreshrate
 $historyStartTime = Util::createTimeForWholeInterval($historyUpdateRate);
 
