401b1407a99f42c10f1ce07e2f551e68d3dcae19
martindiphoorn <martindiphoorn@6945d078-fe45-42e2-ba59-270ac4789834>
2013-06-21 08:29:07 +0000

    Issue #81

---
+++ classes/exceptions/SetupException.php
@@ -0,0 +1,18 @@
+<?php 
+class SetupException extends Exception {
+	// Redefine the exception so message isn't optional
+	public function __construct($message, $code = 0, Exception $previous = null) {
+		// make sure everything is assigned properly
+		parent::__construct($message, $code, $previous);
+	}
+	
+	// custom string representation of object
+	public function __toString() {
+		return $this->message . " in file " . $this->getFile() . '[' . $this->getLine() . ']' ;
+	}
+	
+	public function getJSONMessage() {
+		return array('result'=>'error', 'success'=>false,'exception'=>__CLASS__, 'message'=>$this->__toString());
+	}
+}
+?>
