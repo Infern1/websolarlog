6c9b8f0a69956660ca2d490900826624a99251ac
freemann01 <freemann01@6945d078-fe45-42e2-ba59-270ac4789834>
2013-11-08 16:38:56 +0000

    issue #0000036; try to fix Kostal Piko

--- classes/converters/KostalPikoConverter.php
+++ classes/converters/KostalPikoConverter.php
@@ -20,7 +20,8 @@ class KostalPikoConverter
         foreach ($dataLines as $line){
         	$data[] = preg_split("/[,]+/",$line);
         }
-        HookHandler::getInstance()->fire("onDebug", print_r($data,true));
+        HookHandler::getInstance()->fire("onDebug", "KostalPikoConverterData:".print_r($data,true));
+        
         // Check if the record is okay
         if (!empty($data[22]) && trim($data[22]) != "OK") {
         	throw new ConverterException("not a OK response from KostalPiko:\r\n".print_r($inputLine,true));
@@ -34,43 +35,43 @@ class KostalPikoConverter
         	$result =  Util::getTimestampOfDate($matches[4],$matches[5],$matches[6],$matches[3],$matches[2],$matches[1]);
             $live->time = $result;
         }
-        if (count($data[6] == 6)) {
-            $live->I1V = $data[6][1];
-            $live->I1A = $data[6][2];
-            $live->I1P = $data[6][3];
-        }
         if (count($data[7] == 6)) {
-            $live->I2V = $data[7][1];
-            $live->I2A = $data[7][2];
-            $live->I2P = $data[7][3];
+            $live->I1V = $data[7][1];
+            $live->I1A = $data[7][2];
+            $live->I1P = $data[7][3];
+        }
+        if (count($data[8] == 6)) {
+            $live->I2V = $data[8][1];
+            $live->I2A = $data[8][2];
+            $live->I2P = $data[8][3];
         }
         /**
          *** Third string fix
          */
-        if (count($data[8] == 6)) {
-        	$live->I3V = $data[8][1];
-        	$live->I3A = $data[8][2];
-        	$live->I3P = $data[8][3];
+        if (count($data[9] == 6)) {
+        	$live->I3V = $data[9][1];
+        	$live->I3A = $data[9][2];
+        	$live->I3P = $data[9][3];
         }
         
         
-        if (count($data[9] == 6)) {
-            $live->GV = $data[9][1];
-            $live->GA = $data[9][2];
-            $live->GP = $data[9][3];
+        if (count($data[10] == 6)) {
+            $live->GV = $data[10][1];
+            $live->GA = $data[10][2];
+            $live->GP = $data[10][3];
         }
         /**
          *** 3phase fix
          */
-        if (count($data[10] == 6)) {
-        	$live->GV2 = $data[10][1];
-        	$live->GA2 = $data[10][2];
-        	$live->GP2 = $data[10][3];
-        }
         if (count($data[11] == 6)) {
-        	$live->GV3 = $data[11][1];
-        	$live->GA3 = $data[11][2];
-        	$live->GP3 = $data[11][3];
+        	$live->GV2 = $data[11][1];
+        	$live->GA2 = $data[11][2];
+        	$live->GP2 = $data[11][3];
+        }
+        if (count($data[12] == 6)) {
+        	$live->GV3 = $data[12][1];
+        	$live->GA3 = $data[12][2];
+        	$live->GP3 = $data[12][3];
         }
         
         $live->BOOT = 0;
@@ -89,8 +90,8 @@ class KostalPikoConverter
         	$live->EFF = (float)round(($live->ACP/$live->IP)*100,4);
         }
 
-        if (!empty ($data[4][1])) {
-            $live->KWHT = $data[4][1]/1000;
+        if (!empty ($data[5][1])) {
+            $live->KWHT = $data[5][1]/1000;
         }
         
         // This line is only valid if GP and KWHT are filled with data
@@ -98,7 +99,7 @@ class KostalPikoConverter
         	//HookHandler::getInstance()->fire("onDebug", "Piko.py didn't return KWHT or GP is empty! We need these values for calculations");
         	return null;
         }
-        
+        HookHandler::getInstance()->fire("onDebug", "Live data by PikoKostalConverter\r\n".print_r($live,true));        
         return $live;
     }
     
