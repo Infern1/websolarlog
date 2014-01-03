5c56be6f0f5ca81611b0ee2f6e06cc21020f029e
freemann01 <freemann01@6945d078-fe45-42e2-ba59-270ac4789834>
2014-01-03 14:10:50 +0000

    issue 0000175;

--- addon/PlugwiseStretchAddon.php
+++ addon/PlugwiseStretchAddon.php
@@ -14,6 +14,7 @@ class PlugwiseStretchAddon {
 		if($this->stretchID && $this->stretchIP){
 			if (filter_var($this->stretchIP, FILTER_VALIDATE_IP)) {
 				$this->getPlugsWatts();
+				HookHandler::getInstance()->fire("onInfo", "PlugwiseStretchAddon::onJob run");
 			}	
 		}
 	}
