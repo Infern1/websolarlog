5ef59a6ae932470dba9d8e7fb70686955e53189f
freemann01 <freemann01@6945d078-fe45-42e2-ba59-270ac4789834>
2014-01-30 16:27:58 +0000

    issue #0000036; small fix

--- details.php
+++ details.php
@@ -2,9 +2,10 @@
 require_once("classes/classloader.php");
 Session::initializeLight();
 
-$template = Session::getConfig()->template;
-require_once("template/" . $template . "/header.php");
-require_once("template/" . $template . "/index.php");
+$config = Session::getConfig();
+
+require_once("template/" .  $config->template . "/header.php");
+require_once("template/" .  $config->template . "/index.php");
 $date = Common::getValue('date', 0);
 ?>
 	<script type="text/javascript">
