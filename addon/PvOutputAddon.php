373a6772ddc4ceea00db6c95e2609b90f2058e8d
freemann01 <freemann01@6945d078-fe45-42e2-ba59-270ac4789834>
2014-01-30 15:02:14 +0000

    issue #0000036; small fix

--- addon/PvOutputAddon.php
+++ addon/PvOutputAddon.php
@@ -66,7 +66,56 @@ class PvOutputAddon {
 						if(count($live)>=1){
 							$live->pvoutput = 2;
 							$live->pvoutputErrorMessage = $result['response'];
-							$this->history->save($live);
+							
+							
+							$object = new History();
+							$object->id = $live->id;
+							$object->INV = $live->INV;
+							$object->deviceId = $live->deviceId;
+							$object->SDTE = $live->SDTE;
+							$object->time = $live->time;
+							$object->dayNum = $live->dayNum;
+							
+							$object->I1V = $live->I1V;
+							$object->I1A = $live->I1A;
+							$object->I1P = $live->I1P;
+							$object->I1Ratio = $live->I1Ratio;
+							
+							$object->I2V = $live->I2V;
+							$object->I2A = $live->I2A;
+							$object->I2P = $live->I2P;
+							$object->I2Ratio = $live->I2Ratio;
+							
+							$object->I3V = $live->I3V;
+							$object->I3A = $live->I3A;
+							$object->I3P = $live->I3P;
+							$object->I3Ratio = $live->I3Ratio;
+							
+							$object->GV = $live->GV;
+							$object->GA = $live->GA;
+							$object->GP = $live->GP;
+							
+							$object->GV2 = $live->GV2;
+							$object->GA2 = $live->GA2;
+							$object->GP2 = $live->GP2;
+							
+							$object->GV3 = $live->GV3;
+							$object->GA3 = $live->GA3;
+							$object->GP3 = $live->GP3;
+							
+							$object->IP = $live->IP;
+							$object->ACP = $live->ACP;
+							
+							$object->FRQ = $live->FRQ;
+							$object->EFF = $live->EFF;
+							$object->INVT = $live->INVT;
+							$object->BOOT = $live->BOOT;
+							$object->KWHT = $live->KWHT;
+							$object->pvoutput = $live->pvoutput;
+							$object->pvoutputErrorMessage = $live->pvoutputErrorMessage;
+							$object->pvoutputSend = $live->pvoutputSend;
+							
+							$this->history->save($object);
 						}
 					}else{
 						HookHandler::getInstance()->fire("onDebug", "http_code:unknown:\r\n".print_r($result,true));
