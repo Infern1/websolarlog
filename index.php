4cc25cd9a4debeeb568a84a7a94f1d6209601ad3
freemann01 <freemann01@6945d078-fe45-42e2-ba59-270ac4789834>
2014-02-04 15:51:57 +0000

    issue 0000189; try to prevent "flooding" the device.

--- index.php
+++ index.php
@@ -12,18 +12,27 @@ require_once("template/" .  $config->template . "/index.php");
     
     // every minute
     var sidebar = window.setInterval(function(){WSL.init_PageIndexTotalValues("#sidebar");}, 60000);
-	// every 5 seconds
-	var indexLiveInverters = window.setInterval(function(){WSL.init_PageIndexLiveValues("#indexLiveInverters");}, 5000);
+
 	 
 	$(function(){
 		WSL.init_mainSummary("#main-middle");
 		WSL.init_tabs("index",0, "#main-middle",
 			function(){
 				WSL.init_PageLiveValues("#content",function(){indexLiveInverters});
+
+				$(function(){
+					// set timeout to prevent flooding the device
+					setTimeout(function() {
+						// every 5 seconds
+						var indexLiveInverters = window.setInterval(function(){WSL.init_PageIndexLiveValues("#indexLiveInverters");}, 5000);
+					},2000);
+				});	
+				
 				WSL.init_PageIndexTotalValues("#sidebar",function(){sidebar});
 				analyticsJSCodeBlock();
     		}
-		)	
+		)
+		
 	});
 
 			
