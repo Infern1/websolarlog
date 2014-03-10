5f85f021914dd0caff926b40f5cd0145edab9837
freemann01 <freemann01@6945d078-fe45-42e2-ba59-270ac4789834>
2014-03-10 19:25:10 +0000

    issue #0000036; try to fix nasty bug :|

--- classes/services/HistoryService.php
+++ classes/services/HistoryService.php
@@ -2,6 +2,17 @@
 class HistoryService {
 	public static $tbl = "history";
 	
+	function __construct() {
+		HookHandler::getInstance()->add("onJanitorDbCheck", "HistoryService.janitorDbCheck");
+	}
+	
+
+	public function janitorDbCheck() {
+		HookHandler::getInstance()->fire("onDebug", "HistorySmartMeterService janitor DB Check");
+		// We reasonly discovered a bug which created NULL records in the history table, so we need to delete these records.
+		R::exec("DELETE FROM 'history' WHERE deviceId is null");
+	}
+	
 	/**
 	 * Save the object to the database
 	 * @param History $object
