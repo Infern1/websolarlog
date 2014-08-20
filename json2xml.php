8adc0af32f4b23749f3f5aaf2c20bfeb15042aa5
freemann01 <freemann01@6945d078-fe45-42e2-ba59-270ac4789834>
2014-08-20 21:35:17 +0000

    #0000268 add an option to convert every server or API that return JSON to XML usage: http://www.frijmann.nl/websolarlog/json2xml.php?url=http://www.frijmann.nl/websolarlog/api.php/Live or http://www.frijmann.nl/websolarlog/json2xml.php?url=http://www.frijmann.nl/websolarlog/server.php?method=getPageLiveValues

---
+++ json2xml.php
@@ -0,0 +1,22 @@
+<?php
+// Require Array2XML class which takes a PHP array and changes it to XML
+require_once('utils/array2xml.php');
+
+// Gets JSON file
+$curlSession = curl_init();
+curl_setopt($curlSession, CURLOPT_URL, $_GET['url']);
+curl_setopt($curlSession, CURLOPT_BINARYTRANSFER, true);
+curl_setopt($curlSession, CURLOPT_RETURNTRANSFER, true);
+
+$json = curl_exec($curlSession);
+curl_close($curlSession);
+
+// Decodes JSON into a PHP array
+$php_array['container'] = json_decode($json, true);
+
+// adding Content Type
+
+// Converts PHP Array to XML with the root element being 'root-element-here'
+$xml = Array2XML::createXML('websolarlog', $php_array);
+echo $xml->saveXML();
+?>
