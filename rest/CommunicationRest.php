864639b72a617a84ce365ea00a84b086a4d12da8
freemann01 <freemann01@6945d078-fe45-42e2-ba59-270ac4789834>
2014-12-31 14:10:16 +0000

    - add time to communiocation text "added"

--- rest/CommunicationRest.php
+++ rest/CommunicationRest.php
@@ -101,7 +101,7 @@ class CommunicationRest {
 		$item = new QueueItem(time(), "DeviceHandler.handleTest", "deviceId=".$deviceId."|"."communicationId=".$communicationId, false, 0, true);
 		QueueServer::addItemToDatabase($item);
 		
-		return array("results"=>"added,".date('Y-m-d h:i:s', time()), "communicationId"=>$communicationId, "deviceId"=>$deviceId);
+		return array("results"=>"<b>added:</b> ".date('Y-m-d H:i:s', time()), "communicationId"=>$communicationId, "deviceId"=>$deviceId);
 	}
 }
 ?>
