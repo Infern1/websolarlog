a95cfac56dcb7ad129a782e05c23f54c11cc415d
freemann01 <freemann01@6945d078-fe45-42e2-ba59-270ac4789834>
2015-01-10 10:31:09 +0000

    - fix minderGas API url

--- classes/services/minderGasService.php
+++ classes/services/minderGasService.php
@@ -1,41 +1,39 @@
 <?php
 class minderGasService {
-	public static $tbl = "weather";
+	public static $tbl = "energySmartMeter";
 	private $config;
 	
 	function __construct() {
-		HookHandler::getInstance()->add("onJanitorDbCheck", "minderGasNLService.janitorDbCheck");
 		$this->config = Session::getConfig();
 	}
         
 
         public function minderGasCurl(){
+            echo "aaaa";
             foreach ($this->config->devices as $device) {
                 if($device->minderGasAPIToken){
                     $yesterday = Util::getBeginEndDate('yesterday',1);
 
-                    $table = 'energySmartMeter';
+                    
                     $query = ' time > :beginDate AND time < :endDate and deviceId = :deviceId';
                     $parameters = array( ':beginDate' => $yesterday['beginDate'],':endDate'=>$yesterday['endDate'],':deviceId'=>$device->id);
-                    $beans =  R::findOne($table,$query,$parameters);
+                    $beans =  R::findOne(self::$tbl,$query,$parameters);
 
                     $jsonData = json_encode(array("date" => date("Y-m-d",$beans['time']), "reading" => ($beans['gasUsageT']/1000)));
+  
+                    $headerData = array('Content-Type:application/json','Accept:application/json','AUTH-TOKEN:'.$device->minderGasAPIToken );
 
-                    $ch = curl_init("https://www.mindergas.nl/mobile_app/gas_meter_readings");
+                    $ch = curl_init("https://www.mindergas.nl/api/gas_meter_readings");
                     curl_setopt($ch, CURLOPT_HEADER, 0);
                     curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
                     curl_setopt($ch, CURLOPT_POSTFIELDS, $jsonData);
-                    curl_setopt($ch, CURLOPT_HTTPHEADER, array(
-                        'Content-Type:application/json',
-                        'Accept:application/json',
-                        'AUTH-TOKEN:'.$device->minderGasAPIToken 
-                    ));
+                    curl_setopt($ch, CURLOPT_HTTPHEADER, $headerData);
                     curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
                     curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 4); // Connection timeout in seconds
                     curl_setopt($ch, CURLOPT_TIMEOUT, 60); // Transmission timeout in seconds
                     $response = curl_exec($ch);
                     $info = curl_getinfo($ch);
-
+                    
                     $httpResponse = curl_getinfo($ch,CURLINFO_HEADER_OUT);
                     curl_close($ch);
                 }
